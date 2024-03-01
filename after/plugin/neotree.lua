2304dd5bffe6d748470d09879937d96c9fb5a505
timAtSource <tim@tothesource.com>
2024-03-01 13:35:39 -0800

    neotree update

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,4 +1,12 @@
 require("neo-tree").setup {
   close_if_last_window = true,
-  netrw_hijack_behavior = "open_split"
+  netrw_hijack_behavior = "open_split",
+  follow_current_file = {
+    enabled = true
+  },
+  buffers = {
+    follow_current_file = {
+      enabled = true
+    }
+  }
 }
