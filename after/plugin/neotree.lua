f1850bc2695dae3ac3cb3f819b4996f434c1cda0
Tim Standen <timmstanden@gmail.com>
2024-08-17 16:42:07 -0700

    Update neotree config

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,12 +1,14 @@
 require("neo-tree").setup {
   close_if_last_window = true,
   netrw_hijack_behavior = "open_split",
+
   follow_current_file = {
     enabled = true
   },
-  buffers = {
-    follow_current_file = {
-      enabled = true
+  window = {
+    position = "float",
+    mappings = {
+      ["P"] = { "toggle_preview", config = {use_image_nvim = true}}
     }
   }
 }
