fa9cf9a8af9d9eb78cca212666843dbd8bf8325c
Tim Standen <timmstanden@gmail.com>
2025-10-24 14:22:14 -0700

    [F] Add typst

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -71,11 +71,18 @@ return require('lazy').setup({
             only_render_image_at_cursor = true,
             filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
           },
+          typst = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = true,
+            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+          },
           neorg = {
             enabled = true,
             clear_in_insert_mode = false,
             download_remote_images = true,
-            only_render_image_at_cursor = false,
+            only_render_image_at_cursor = true,
             filetypes = { "norg" },
           },
           html = {
