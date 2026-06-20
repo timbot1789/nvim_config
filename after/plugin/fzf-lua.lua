2cb781c8f9b7b6e3517be08e8cba71b866f0a76a
Tim Standen <tim.standen@gusto.com>
2026-06-19 17:07:42 -0700

    Update fzf-lua config

--- after/plugin/fzf-lua.lua
+++ after/plugin/fzf-lua.lua
@@ -1,8 +1,15 @@
 require('fzf-lua').setup({
-  treesitter = {
-    enabled = false
+  winopts ={
+    treesitter = {
+      enabled = false
+    },
+    preview = {
+      flip_columns = 190
+    },
   },
-  preview = {
-    flip_columns = 60
+  files = {
+    formatter = "path.filename_first",
+    find_opts = [[-type f \! -path '*/.git/*' \! -path '*/.jj/*' \! -path '**/.rbi']],
+    rg_opts = [[--color=never --files -g "!.git" -g "!.jj" -g "!.rbi"]],
   }
 })
