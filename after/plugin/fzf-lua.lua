730d36a5e39ab9ebacab357f3180f081f251fce8
Tim Standen <tim.standen@gusto.com>
2026-06-19 17:10:23 -0700

    Fix file search to ignore sorbet

--- after/plugin/fzf-lua.lua
+++ after/plugin/fzf-lua.lua
@@ -11,5 +11,7 @@ require('fzf-lua').setup({
     formatter = "path.filename_first",
     find_opts = [[-type f \! -path '*/.git/*' \! -path '*/.jj/*' \! -path '**/.rbi']],
     rg_opts = [[--color=never --files -g "!.git" -g "!.jj" -g "!.rbi"]],
+    -- File search uses fd most of the time
+    fd_opts = [[--color=never --type f --type l --exclude .git --exclude .jj --exclude rbi]],
   }
 })
